f65af362bbc87115b4c1b8567271e2c80d7eb274
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-04-23 09:07:48 +0530

    first commit

--- dream_career_oracle/dream_career_oracle/settings.py
+++ dream_career_oracle/dream_career_oracle/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "dreams",
-    
 ]
 
 MIDDLEWARE = [
@@ -75,12 +80,12 @@ WSGI_APPLICATION = "dream_career_oracle.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
 }
 
+print("ENV FILE LOADED:", os.getenv("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
