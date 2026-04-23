c18209235114f48f012d376e0d819a77384d657a
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-04-23 09:26:18 +0530

    first commit

--- dream_career_oracle/dream_career_oracle/settings.py
+++ dream_career_oracle/dream_career_oracle/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "dream_career_oracle.urls"
@@ -117,6 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_URL = "static/"
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # your static folder
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # for production
